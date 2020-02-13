43ed05b16df3021ae23dd716c7076e8d59096621
trobiuk <trobiuk@gmail.com>
2020-02-13 22:12:05 +0200

    added new-products and slider-followers section

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ $(function() {
         spacing: "3px"
     });
 
-    $(".active-star").rateYo({
+    $(".product-item__info-star.active-star").rateYo({
         starWidth: "12px",
         rating: 4.5,
         readOnly: true,
@@ -24,6 +24,14 @@ $(function() {
     });
 
     var mixer = mixitup('.new-products__list');
+
+    $('.slider-followers__inner').slick({
+        appendArrows: $('.slider-followers__box-top-btn'),
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/chevron-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/chevron-right.svg" alt=""></button>',
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 
 
 

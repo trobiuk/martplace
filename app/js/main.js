0eeadcd4dee030afec2c288ea29a71f65e231f99
trobiuk <trobiuk@gmail.com>
2020-02-13 09:24:54 +0200

    change class name in slider-featured

--- app/js/main.js
+++ app/js/main.js
@@ -2,14 +2,14 @@ $(function() {
 
     $('.search__box select').styler();
 
-    $(".slider-featured__item-star").rateYo({
+    $(".slider-featured__item-description-functional-star").rateYo({
         starWidth: "14px",
         rating: 4.5,
         readOnly: true
     });
 
     $('.slider-featured__inner').slick({
-        appendArrows: $('.slider-featured__btn'),
+        appendArrows: $('.slider-featured__box-top-btn'),
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/chevron-left.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/chevron-right.svg" alt=""></button>'
     });
